8bca3defa17bb37a9fca980f7d7ef82af645349c
Sergio Santiago <sergio.santiago@doctolib.com>
2025-07-14 11:03:38 +0200

    update yak-gui to v1.0.1 with correct SHA256 from release

--- Formula/yak-gui.rb
+++ Formula/yak-gui.rb
@@ -1,17 +1,17 @@
 class YakGui < Formula
   desc "Desktop GUI for yak CLI tool"
   homepage "https://github.com/santi1s/yak"
-  version "1.0.0"
+  version "1.0.1"
 
   url "https://github.com/santi1s/yak/releases/download/v#{version}/yak-gui-darwin-universal.tar.gz"
-  sha256 "2e377f9fd46c3bcdf1f0195fdab9e0f5cec1f27743b5af5d453e584ad5c0917d"
+  sha256 "4842fcd2f6f265ccf7e63eff09621834a70c2744f9dcce1cbcf4f952dab8b2bf"
 
   def install
     # Debug: show what files are available
     system "pwd"
     system "ls", "-la"
     system "find", ".", "-name", "*.app"
-    
+
     prefix.install "yak-gui.app"
     bin.write_exec_script prefix/"yak-gui.app/Contents/MacOS/yak-gui"
   end
@@ -20,7 +20,7 @@ class YakGui < Formula
     <<~EOS
       yak-gui has been installed to:
         #{prefix}/yak-gui.app
-      
+
       You can run it from the command line with: yak-gui
       Or open it directly: open "#{prefix}/yak-gui.app"
     EOS
