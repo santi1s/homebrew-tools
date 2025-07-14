4deb03b260e315cb96f33a87c18c27327215e933
Sergio Santiago <sergio.santiago@doctolib.com>
2025-07-14 10:32:14 +0200

    fix yak-gui formula to use prefix.install instead of app.install

--- Formula/yak-gui.rb
+++ Formula/yak-gui.rb
@@ -7,13 +7,17 @@ class YakGui < Formula
   sha256 "2e377f9fd46c3bcdf1f0195fdab9e0f5cec1f27743b5af5d453e584ad5c0917d"
 
   def install
-    app.install "yak-gui.app"
+    prefix.install "yak-gui.app"
+    bin.write_exec_script prefix/"yak-gui.app/Contents/MacOS/yak-gui"
   end
 
   def caveats
     <<~EOS
-      yak-gui has been installed to /Applications.
-      You can launch it from Launchpad or run: open -a yak-gui
+      yak-gui has been installed to:
+        #{prefix}/yak-gui.app
+      
+      You can run it from the command line with: yak-gui
+      Or open it directly: open "#{prefix}/yak-gui.app"
     EOS
   end
 end
