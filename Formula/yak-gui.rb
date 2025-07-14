aa3dddd508c12a6253e35ada0b22333f47aa2af9
Sergio Santiago <sergio.santiago@doctolib.com>
2025-07-14 10:29:26 +0200

    simplify yak-gui formula to use app.install

--- Formula/yak-gui.rb
+++ Formula/yak-gui.rb
@@ -6,28 +6,14 @@ class YakGui < Formula
   url "https://github.com/santi1s/yak/releases/download/v#{version}/yak-gui-darwin-universal.tar.gz"
   sha256 "2e377f9fd46c3bcdf1f0195fdab9e0f5cec1f27743b5af5d453e584ad5c0917d"
 
-
   def install
-    # Install the .app bundle to Applications directory
-    (prefix/"Applications").mkpath
-    cp_r "yak-gui.app", "#{prefix}/Applications/"
-    
-    # Create symlink for command-line access
-    bin.write_exec_script "#{prefix}/Applications/yak-gui.app/Contents/MacOS/yak-gui"
+    app.install "yak-gui.app"
   end
 
   def caveats
     <<~EOS
-      yak-gui has been installed to:
-        #{prefix}/Applications/yak-gui.app
-
-      You can run it from the command line with: yak-gui
-      Or open it directly: open "#{prefix}/Applications/yak-gui.app"
+      yak-gui has been installed to /Applications.
+      You can launch it from Launchpad or run: open -a yak-gui
     EOS
-  end
-
-  test do
-    # Test that the app bundle exists
-    assert_predicate prefix/"Applications/yak-gui.app", :exist?
   end
 end
