1e1afd3aa4290f845f0bffbcfb2c91bade936cdb
Sergio Santiago <sergio.santiago@doctolib.com>
2025-07-15 22:18:49 +0200

    bump yak-gui version

--- Casks/yak-gui.rb
+++ Casks/yak-gui.rb
@@ -1,5 +1,5 @@
 cask "yak-gui" do
-  version "1.6.0"
+  version "1.10.1"
   sha256 :no_check
 
   url "https://github.com/santi1s/yak-gui/releases/download/v#{version}/yak-gui-darwin-universal.tar.gz"
