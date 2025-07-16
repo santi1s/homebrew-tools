56149fc89a457353f91fcd317d4ded18f12ee789
Sérgio Santiago e Santiago <52079665+santi1s@users.noreply.github.com>
2025-07-16 11:26:13 +0200

    Update yak-gui to 1.14.1

--- Casks/yak-gui.rb
+++ Casks/yak-gui.rb
@@ -1,5 +1,5 @@
 cask "yak-gui" do
-  version "1.14.0"
+  version "1.14.1"
   sha256 :no_check
 
   url "https://github.com/santi1s/yak-gui/releases/download/v#{version}/yak-gui-darwin-universal.tar.gz"
